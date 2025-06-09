6f7ff2cbc153915fee67e8672f36c34c9e5352e3
pranjalkumarmall <pranjal18mall@gmail.com>
2025-06-09 23:28:05 +0530

    Update useGetExpenses.jsx

--- frontend/src/hooks/useGetExpenses.jsx
+++ frontend/src/hooks/useGetExpenses.jsx
@@ -1,27 +1,33 @@
 import { useEffect } from "react";
-import  {useDispatch,useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setExpenses } from "../redux/expenseSlice";
 
+// Set this once globally, or you can do it here once
+axios.defaults.withCredentials = true;
 
-const useGetExpenses=()=>{
-    const dispatch=useDispatch();
-    const {category,markAsDone}=useSelector(store=>store.expense);
+const useGetExpenses = () => {
+  const dispatch = useDispatch();
+  const { category, markAsDone } = useSelector(store => store.expense);
 
-    useEffect(()=>{
-        const fetchExpense=async()=>{
-            try{
-                axios.defaults.withCredentials=true;
-            const res=  await  axios.get(`https://expense-tracker-1qkr.onrender.com/api/v1/expense/getall?category=${category}&done=${markAsDone}`
-);
-              if(res.data.success){
-                dispatch(setExpenses(res.data.expense));
-              }
-            } catch(error){
-                console.log(error);
-            }
+  useEffect(() => {
+    const fetchExpense = async () => {
+      try {
+        const res = await axios.get(
+          `https://expense-tracker-1qkr.onrender.com/api/v1/expense/getall?category=${category}&done=${markAsDone}`
+        );
+
+        if (res.data.success) {
+          dispatch(setExpenses(res.data.expense));
         }
-        fetchExpense();
-    },[dispatch,category,markAsDone]);
-}
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchExpense();
+  }, [dispatch, category, markAsDone]);
+};
+
 export default useGetExpenses;
+
